b550931332b431bfe5cdf3e0ac54bf2d881bb274
Dave Blencowe <dblencowe@gmail.com>
2023-07-15 09:34:28 +0100

    fix: adds s3 bucket ownership controls to allow ACL creation

--- modules/static-site/s3.tf
+++ modules/static-site/s3.tf
@@ -18,9 +18,17 @@ resource "aws_s3_bucket_acl" "website" {
     bucket = aws_s3_bucket.website.id
 
     acl = "private"
+    depends_on = [aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership]
 }
 
 resource "aws_s3_bucket_policy" "website" {
     bucket = aws_s3_bucket.website.id
-    policy = data.aws_iam_policy_document.website_policy.json 
+    policy = data.aws_iam_policy_document.website_policy.json
+}
+
+resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
+  bucket = aws_s3_bucket.website.id
+  rule {
+    object_ownership = "ObjectWriter"
+  }
 }
